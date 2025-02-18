605e95ab4568e534cfbcdffbcb6fa583d6dd20d0
Matteo Cusanelli <124231840+matt200500@users.noreply.github.com>
2025-02-17 21:14:45 -0700

    Time: 20 ms (26.44%) | Memory: 14.3 MB (30.63%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,32 +2,17 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         std::unordered_set<char> current_string;
-        std::queue<char> current_substring;
+        int left_index = 0;
         int max_size = 0;
-        int current_size = 0;
         for (int i = 0; i < s.size(); ++i){
-            if (current_string.find(s[i]) == current_string.end()){ // not present
-                current_string.insert(s[i]);
-                current_substring.push(s[i]);
-                ++current_size;
-                if (current_size > max_size){
-                    max_size = current_size;
-                }
-            }else{
-                bool contains_item = true;
-                current_substring.push(s[i]);
-                ++current_size;
-                while(contains_item){
-                    if (current_substring.front() == s[i]){
-                        contains_item = false;
-                    }else{
-                        current_string.erase(current_substring.front());
-                    }
-                    current_substring.pop();
-                    --current_size;
-                }  
+            while (current_string.find(s[i]) != current_string.end()){ //  present
+                current_string.erase(s[left_index]);
+                ++left_index;
             }
+            current_string.insert(s[i]);
+
+            max_size = max((i+1)-left_index,max_size);
         }
-        return max(current_size, max_size);
+        return max_size;
     }
 };
