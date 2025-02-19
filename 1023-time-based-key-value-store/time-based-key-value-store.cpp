d2ec58653a5455e36ce0ddc04dff9aac2b3adf7a
Matteo Cusanelli <124231840+matt200500@users.noreply.github.com>
2025-02-19 00:48:56 -0700

    Time: 98 ms (8.69%) | Memory: 137 MB (48.07%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -15,24 +15,27 @@ private:
     unordered_map<string, vector<pair<int, string>>> timeMap;
 
     std::string binary_search(string key, int start, int end, int target, int next_largest){
-        int middle = start + (end - start) /2;
-        if (timeMap[key].size() == 0){
-            return "";
-        }
-        if (start > end){
-            if (next_largest != -1){
-                return timeMap[key][next_largest].second;
-            }else{
+        while(start <= end){
+            int middle = start + (end - start) /2;
+            if (timeMap[key].size() == 0){
                 return "";
             }
+
+            if(timeMap[key][middle].first == target){
+                return timeMap[key][middle].second;
+            }
+            if(timeMap[key][middle].first < target){
+                next_largest = middle;
+                start = middle+1;
+            }else{
+                end = middle -1;
+            }
         }
-        if(timeMap[key][middle].first == target){
-            return timeMap[key][middle].second;
-        }
-        if(timeMap[key][middle].first < target){
-            return binary_search(key, middle+1, end, target, middle);
+        
+        if (next_largest != -1){
+            return timeMap[key][next_largest].second;
         }else{
-            return binary_search(key, start, middle -1, target, next_largest);
+            return "";
         }
     }
 };
