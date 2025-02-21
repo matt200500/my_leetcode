ea4f6eace93f1a4432e409926ff9580114d3382a
Matteo Cusanelli <124231840+matt200500@users.noreply.github.com>
2025-02-20 23:24:35 -0700

    Time: 1 ms (41.54%) | Memory: 16.7 MB (82.49%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int minutes = 0;
+        queue<tuple<int,int>> q;
+        for (int row = 0; row < grid.size(); ++row){
+            for (int column = 0; column < grid[0].size(); ++column){
+                if(grid[row][column] == 2){
+                    q.push({row, column});
+                }
+            }
+        }
+        BFS(grid, minutes, q);
+
+        for (int row = 0; row < grid.size(); ++row){
+            for (int column = 0; column < grid[0].size(); ++column){
+                if(grid[row][column] == 1){
+                    return -1;
+                }
+            }
+        }
+        return minutes;
+    }
+
+private:
+    void BFS(vector<vector<int>>& grid, int& minutes, queue<tuple<int,int>>& q){
+        while(!q.empty()){
+            bool rotted = false;
+            std::vector<tuple<int,int>> next_level;
+
+            while(!q.empty()){
+                int current_row = get<0>(q.front());
+                int current_column = get<1>(q.front());
+                q.pop();
+
+                if (current_row < 0 || current_row > grid.size() -1){
+                    continue;
+                }
+                if (current_column < 0 || current_column > grid[0].size() -1){
+                    continue;
+                }
+                if (grid[current_row][current_column] == 0){
+                    continue;
+                }
+
+                if (grid[current_row][current_column] == 1){
+                    grid[current_row][current_column] = 2;
+                }
+                
+                if (current_row + 1 < grid.size() && grid[current_row + 1][current_column] == 1) {
+                    grid[current_row + 1][current_column] = 2;
+                    next_level.push_back({current_row + 1, current_column});
+                    rotted = true;
+                }
+                if (current_row - 1 >= 0 && grid[current_row - 1][current_column] == 1) {
+                    grid[current_row - 1][current_column] = 2;
+                    next_level.push_back({current_row - 1, current_column});
+                    rotted = true;
+                }
+                if (current_column + 1 < grid[0].size() && grid[current_row][current_column + 1] == 1) {
+                    grid[current_row][current_column + 1] = 2;
+                    next_level.push_back({current_row, current_column + 1});
+                    rotted = true;
+                }
+                if (current_column - 1 >= 0 && grid[current_row][current_column - 1] == 1) {
+                    grid[current_row][current_column - 1] = 2;
+                    next_level.push_back({current_row, current_column - 1});
+                    rotted = true;
+                }
+            }
+
+            for (int i = 0; i < next_level.size(); ++i){
+                q.push(next_level[i]);
+            }
+            if (rotted == true){
+                ++minutes;
+            }    
+        }
+        
+    }
+};
