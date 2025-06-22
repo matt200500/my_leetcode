b5a4fa8de09a90ad3ff8ba936fcfc5b9c4630f19
Matteo Cusanelli <124231840+matt200500@users.noreply.github.com>
2025-06-21 22:01:35 -0600

    Time: 170 ms (5.13%) | Memory: 26.1 MB (18.30%) - LeetSync

---
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        std::vector<int> result(n, 0);
+        std::stack<int> running_programs;
+        int previous_time = 0;
+
+        for(int i =0; i < logs.size(); ++i){
+            std::stringstream log_stream(logs[i]);
+            std::vector<string> parts;
+            std::string part;
+
+            while(!log_stream.eof()){
+                getline(log_stream,part, ':');
+                parts.push_back(part);
+            }
+
+            int id_number = std::stoi(parts[0]);
+            std::string call = parts[1];
+            int timestamp = std::stoi(parts[2]);
+            cout << "id: " << id_number << " call: " << call << " timestamp: " << timestamp << "\n";
+
+            if (call == "start"){
+                if(!running_programs.empty()){
+                    result[running_programs.top()] += timestamp - previous_time;
+                }
+                running_programs.push(id_number);
+                previous_time = timestamp;
+            }else{
+                result[running_programs.top()] += timestamp - previous_time + 1;
+                running_programs.pop();
+                previous_time = timestamp + 1;
+            }
+
+        }
+
+        return result;
+    }
+};
