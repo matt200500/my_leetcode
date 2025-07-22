c11334ef2013ec696e20c55b63684c75f139853f
Matteo Cusanelli <124231840+matt200500@users.noreply.github.com>
2025-07-21 21:37:22 -0600

    Time: 1037 ms (6.94%) | Memory: 483.8 MB (5.00%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        std::string longest_palindrome;
+        longest_palindrome = s[0];
+        for (int i = 0; i < s.length(); ++i){
+            std::string current_palindrome;
+            current_palindrome = s[i];
+            int left_char = i-1;
+            int right_char = i+1;
+            // cout << "current char " << s[i] << "\n";
+            bool all_same = true;
+            while(right_char < s.length()){
+                if(left_char >= 0 && s[left_char] == s[right_char]){
+                    if (s[left_char] != s[i]){
+                        all_same = false;
+                    }
+                    current_palindrome = s.substr(left_char, right_char - left_char + 1);
+                    // cout << "current palindrome 1" << current_palindrome << "\n";
+
+                    if(current_palindrome.length() > longest_palindrome.length()){
+                        longest_palindrome = current_palindrome;
+                    }
+
+                    --left_char;
+                    ++right_char;
+
+                } else if ( all_same == true && s[right_char] == s[i]){
+                    current_palindrome = s.substr(i, right_char - i + 1);
+                    // cout << "current palindrome 2" << current_palindrome << "\n";
+
+                    if(current_palindrome.length() > longest_palindrome.length()){
+                        longest_palindrome = current_palindrome;
+                    }
+                    ++right_char;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        return longest_palindrome;
+    }
+};
