0ec2065a9fee2a104f9403b00c4492cedc70837a
Matteo Cusanelli <124231840+matt200500@users.noreply.github.com>
2025-07-21 21:45:27 -0600

    Time: 11 ms (68.95%) | Memory: 9.3 MB (82.14%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        std::string longest_palindrome;
-        longest_palindrome = s[0];
+        int start = 0;
+        int end = 0;
+
         for (int i = 0; i < s.length(); ++i){
-            std::string current_palindrome;
-            current_palindrome = s[i];
             int left_char = i-1;
             int right_char = i+1;
             bool all_same = true;
+
             while(right_char < s.length()){
                 if(left_char >= 0 && s[left_char] == s[right_char]){
                     if (s[left_char] != s[i]){
                         all_same = false;
                     }
-                    current_palindrome = s.substr(left_char, right_char - left_char + 1);
-                    if(current_palindrome.length() > longest_palindrome.length()){
-                        longest_palindrome = current_palindrome;
+                    if((right_char - left_char + 1) > (end - start +1)){
+                        start = left_char;
+                        end = right_char;
                     }
 
                     --left_char;
                     ++right_char;
 
                 } else if ( all_same == true && s[right_char] == s[i]){
-                    current_palindrome = s.substr(i, right_char - i + 1);
 
-                    if(current_palindrome.length() > longest_palindrome.length()){
-                        longest_palindrome = current_palindrome;
+                    if((right_char - i + 1) > (end - start +1)){
+                        start = i;
+                        end = right_char;
                     }
                     ++right_char;
                 }
@@ -35,6 +35,6 @@ public:
                 }
             }
         }
-        return longest_palindrome;
+        return s.substr(start, end - start + 1);
     }
 };
