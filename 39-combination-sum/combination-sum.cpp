99602f5ab06373dc1cce9e98a078b875127eb25e
Matteo Cusanelli <124231840+matt200500@users.noreply.github.com>
2025-07-22 21:02:48 -0600

    Time: 5 ms (35.95%) | Memory: 14.1 MB (54.45%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -3,27 +3,26 @@ public:
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         std::vector<vector<int>> output;
         std::vector<int> current_order;
-        depth_first_search(0, target, 0, current_order, nums, output);
+        depth_first_search(0, target, current_order, nums, output);
         return output;
     }
 
 private:
-    void depth_first_search(int root, int target, int current_total, vector<int> current_order, const vector<int>& nums, vector<vector<int>>& output){
+    void depth_first_search(int root, int target, vector<int>& current_order, const vector<int>& nums, vector<vector<int>>& output){
         if (root >= nums.size()){
             return;
         }
-        if (current_total == target){
+        if (target == 0){
             output.push_back(current_order);
             return;
         }
 
-        if (current_total + nums[root] <= target){ // adding root
+        if (target - nums[root] >= 0){ // adding root
             current_order.push_back(nums[root]);
-            int new_total = current_total +  nums[root];
-            depth_first_search(root, target, new_total, current_order, nums, output);
+            depth_first_search(root, target - nums[root], current_order, nums, output);
             current_order.pop_back();
         }
 
-        depth_first_search(root+1, target, current_total, current_order, nums, output);
+        depth_first_search(root+1, target, current_order, nums, output);
     }
 };
