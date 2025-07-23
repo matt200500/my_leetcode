61232b5534324feaa0a671cd1d1d85f95af2387d
Matteo Cusanelli <124231840+matt200500@users.noreply.github.com>
2025-07-22 18:22:37 -0600

    Time: 129 ms (12.24%) | Memory: 100.4 MB (7.66%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-
-    static bool comparison (int a, int b){
-        return a > b;
-    }
-
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         std::stack<double> fleets;
-        std::map<int, int> speed_mapping;
+        std::map<int, int, std::greater<int>> speed_mapping;
         for (int i = 0; i < position.size(); ++i){
            speed_mapping.insert({position[i], speed[i]}); 
         }
-        std::sort(position.begin(), position.end(), comparison);
-        for (int i = 0; i < position.size(); ++i){
-            double time = static_cast<double>((target - position[i])) / speed_mapping[position[i]];
+        for (auto key : speed_mapping){
+            double time = static_cast<double>((target - key.first)) / key.second;
             if (!fleets.empty()){
                 if (time > fleets.top()){
                     fleets.push(time);
